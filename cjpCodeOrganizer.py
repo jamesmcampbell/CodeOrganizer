4fc3e0e74e0bc85206b03a6967ec8fd5d1787a66
jamesmcampbell <58498673+jamesmcampbell@users.noreply.github.com>
2020-09-24 16:54:31 -0700

    Update cjpCodeOrganizer.py

--- cjpCodeOrganizer.py
+++ cjpCodeOrganizer.py
@@ -10,13 +10,14 @@ def parse_brackets(word_list):
 
 	for x in range(0, len(word_list)):
 		word_list[x] = str(word_list[x].strip())
-		if '}' not in word_list[x] and '{' not in word_list[x] and word_list[x] != '':
+		# if neither brackets are in the list
+		if '{' not in word_list[x] and '}' not in word_list[x] and word_list[x] != '':
+            # simply rewrite the line
 			output_file.write('\n' + '\t'*bracket + word_list[x])
 		elif word_list[x] == '}':
 			bracket -= 1
-			# print(bracket)
-			if bracket != 0:
-				output_file.write("\n" *empty_lines)
+			# if bracket != 0:
+				# output_file.write("\n"*empty_lines)
 			output_file.write("\n"*second_bracket + ' '*abs(second_bracket-1) + '}')
 		elif '{' in word_list[x]:
 			bracket += 1
@@ -25,20 +26,8 @@ def parse_brackets(word_list):
 original_file = Path(input("\nWhat is your file's location? (folder/directory path including the file name)"
 "\n\nFor example (for Windows)- C:\\Users\\James\\Documents\\example.txt\n\n"))
 
-style = input("\nWhat coding style do you want? (c, j, p):\n\n"
-"C-Style (c) - Brackets in their own lines"
-"\nJava Style (j) - First bracket following text, second in it's own line"
-"\nPython Style (p) - All brackets following text\n\n")
-
-if style == "c":
-	first_bracket = 1
-	second_bracket = 1
-elif style == "j":
-	first_bracket = 0
-	second_bracket = 1
-elif style == "p":
-	first_bracket = 0
-	second_bracket = 0
+first_bracket = 0
+second_bracket = 0
 
 raw_text = original_file.read_text()
 original_list = raw_text.split('\n')
@@ -47,8 +36,10 @@ output_file = open('new_' + original_file.name, 'w')
 
 for x in range (0, len(original_list)):
 	if len(original_list[x]) != 0:
-		if '}' not in original_list[x] and '{' not in original_list[x]:
+		# if neither brackets are in the list
+		if '{' not in original_list[x] and '}' not in original_list[x]:
 			output_file.write('\n' + '\t'*bracket + original_list[x].strip())
+		#if a bracket is in the list
 		else:
 			line_list = split(r"([{}])", original_list[x])
 			parse_brackets(line_list)
